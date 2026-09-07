c432363c3168a66e472119bb27e9c3a50a0972ae
sarcke1 <128915227+sarcke1@users.noreply.github.com>
2026-09-07 13:07:20 +0200

    Remplacement de la photo des nems

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const dishes=[
 {id:"pad",name:"Pad Thaï",price:11,emoji:"🍜",photo:"photo-pad",desc:"Nouilles de riz sautées, crevettes, œuf, tofu, pousses de soja et cacahuètes.",spicy:true},
 {id:"jour",name:"Plat thaï du jour",price:9,emoji:"🍛",photo:"photo-krapow",desc:"Exemple du jour : Khao Pad Krapow, poulet au basilic thaï, ail et piment, servi avec riz jasmin.",spicy:true},
 {id:"kids",name:"Menu enfant",price:6,emoji:"🍗",photoUrl:"https://images.pexels.com/photos/36879224/pexels-photo-36879224.jpeg?cs=srgb&dl=pexels-mahmut-zeytin-2159832264-36879224.jpg&fm=jpg",desc:"2 pilons de poulet frit + frites, ou 2 nems + riz.",spicy:false},
-{id:"nems",name:"Nems",price:1,emoji:"🥢",photo:"photo-nems",desc:"Nems thaïlandais croustillants, à l’unité.",spicy:false},
+{id:"nems",name:"Nems",price:1,emoji:"🥢",photoUrl:"https://images.pexels.com/photos/12356601/pexels-photo-12356601.jpeg?cs=srgb&dl=pexels-capturedbyaugustine-12356601.jpg&fm=jpg",desc:"Nems thaïlandais croustillants, à l’unité.",spicy:false},
 {id:"samoussa",name:"Samoussa",price:1.5,emoji:"🥟",photoUrl:"https://images.pexels.com/photos/28075291/pexels-photo-28075291.jpeg?cs=srgb&dl=pexels-ars-visions-1267482-28075291.jpg&fm=jpg",desc:"Samoussa croustillant, à l’unité.",spicy:false},
 {id:"spring",name:"Spring roll",price:3,emoji:"🌯",photoUrl:"https://images.pexels.com/photos/840216/pexels-photo-840216.jpeg?cs=srgb&dl=pexels-thngocbich-840216.jpg&fm=jpg",desc:"Rouleaux croustillants servis avec une garniture fraîche.",spicy:false},
 {id:"mango",name:"Sticky rice mangue",price:4,emoji:"🥭",photo:"photo-mango",desc:"Riz gluant au lait de coco et mangue fraîche, selon saison.",spicy:false},
